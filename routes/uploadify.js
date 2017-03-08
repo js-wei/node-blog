97f935078d53ffdaeae297976357d9d590a0a1e6
js-wei <524314430@qq.com>
2017-03-08 10:30:58 +0800

    添加上传头像方法

--- routes/uploadify.js
+++ routes/uploadify.js
@@ -6,6 +6,45 @@ var sd = require('silly-datetime');
 var multiparty = require('multiparty');   //中间件实现上传
 var fs = require('fs');
 
+
+router.post('/upload_archiver',(req,res)=>{
+    var date = sd.format(new Date(), 'YYYYMMDD');
+    path = './public/archiver/'+date+"/";
+    //创建文件夹
+    fs.exists(path,(exists)=>{
+       if(!exists){
+          fs.mkdir(path,(err)=>{
+            if(err){
+              res.send(dstPath);
+              return;
+            }
+          });
+       }
+    });
+    //生成multiparty对象，并配置上传目标路径
+    var form = new multiparty.Form({uploadDir:path});
+    //上传完成后处理
+    form.parse(req, function(err, fields, files) {
+        var filesTmp = JSON.stringify(files,null,2);
+        if(err){
+          console.log('parse error: ' + err);
+        } else {
+          var inputFile = files.image[0];
+          var uploadedPath = inputFile.path;
+          var dstPath = path + inputFile.originalFilename;
+          //重命名为真实文件名
+          fs.rename(uploadedPath, dstPath, function(err) {
+            if(err){
+              console.log('rename error: ' + err);
+            } else {
+              console.log('rename ok');
+            }
+          });
+        }
+        res.send(dstPath.replace('./public',''));
+    });
+});
+
 // var multipartMiddleware = multipart();
 router.post('/uploadimg',(req,res)=>{
     var date = sd.format(new Date(), 'YYYYMMDD');

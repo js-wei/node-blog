22fd42b515d623e21da48891d11d6feaf0e72434
js-wei <524314430@qq.com>
2017-03-22 17:54:36 +0800

    使用vue

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,39 @@
 'use strict'
-var express = require('express'),
+let express = require('express'),
 router = express.Router(),
-helper = require('../model/helper'),
-renderer = require('vue-server-renderer').createRenderer(),
-fs = require('fs'),
-path = require('path');
-global.Vue = require('vue');
+helper = require('../model/helper');
+
+router.use((req,res,next)=>{
+    next();
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next){
-  // let layout = fs.readFileSync('./views/index/index.html', 'utf8');
-  // renderer.renderToString(require('../vue/app')(), function (error, html) {
-  //   if (error) throw error;
-  //
-  // });
-  res.render('index');
+    res.render('index/index');
+})
+.get('/colunm',(req, res)=>{
+  let Colunm = require('../model/colunm');
+  Colunm.find({fid:'',status:true},'_id title name date',(e,r)=>{
+       if(e){
+         res.json({msg:e});
+         return;
+       }
+       res.json(r);
+       return;
+  });
+})
+.get('/marquee',(req,res)=>{
+  let Article = require('../model/article');
+  Article.find({status:false},'_id title name',(e,r)=>{
+       if(e){
+         res.json({msg:e});
+         return;
+       }
+       res.json(r);
+       return;
+  }).sort({date:-1});
+})
+.get('/community',(req,res)=>{
+    res.render('index/list');
 });
-
 module.exports = router;
